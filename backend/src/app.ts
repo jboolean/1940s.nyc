8f3bab42df422105e1e14eb6350798c934346f34
Julian Boilen <julianboilen@gmail.com>
2020-01-05 19:17:15 -0500

    CORS

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,16 @@
 import express from 'express';
 const app = express();
+import cors from 'cors';
 
 import createConnection from './createConnection';
 
 import PhotosResource from './api/PhotosResource';
+
+app.use(
+  cors({
+    origin: ['http://localhost:8080', 'https://1940s.nyc'],
+  })
+);
 
 app.use(async (req, res, next) => {
   await createConnection();
