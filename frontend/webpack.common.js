412214f7715f37ab979b9931858d448ce1fd9383
Julian Boilen <julianboilen@gmail.com>
2020-08-09 18:28:34 -0400

    Copy redirects file to dist

--- frontend/webpack.common.js
+++ frontend/webpack.common.js
@@ -4,6 +4,7 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 // Constant with our paths
 const paths = {
@@ -26,6 +27,9 @@ module.exports = {
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
     new HtmlWebpackPlugin({
       template: path.join(path.resolve(__dirname, 'src'), 'app.html'),
+    }),
+    new CopyPlugin({
+      patterns: [{ from: '_redirects' }],
     }),
   ],
   module: {
