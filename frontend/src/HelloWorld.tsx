5b20c0e9b73e1ff737d2309b64c9fff246b2857f
Julian Boilen <julianboilen@gmail.com>
2020-08-10 20:52:11 -0400

    Delete Hello World

--- frontend/src/HelloWorld.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import stylesheet from './HelloWorld.less';
-
-export default class HelloWorld extends React.Component<{}, {}> {
-  render(): JSX.Element {
-    return <h1 className={stylesheet.marquee}>Hello World!</h1>;
-  }
-}
