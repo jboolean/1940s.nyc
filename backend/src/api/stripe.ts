12a6a9c4638f04b29e1bdbf65424de4f21b17f60
Julian Boilen <julianboilen@gmail.com>
2022-12-18 14:12:57 -0500

    Upgrade Stripe version

--- backend/src/api/stripe.ts
+++ backend/src/api/stripe.ts
@@ -3,7 +3,7 @@ import Stripe from 'stripe';
 const { STRIPE_SK } = process.env;
 
 const stripe = new Stripe(STRIPE_SK || '', {
-  apiVersion: '2022-08-01',
+  apiVersion: '2022-11-15',
 });
 
 export default stripe;
