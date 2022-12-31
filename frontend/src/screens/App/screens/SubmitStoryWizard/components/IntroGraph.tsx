40907a17545c5e40791c2c60a8abe84427fe880f
Julian Boilen <julianboilen@gmail.com>
2022-12-30 20:44:41 -0500

    setting! is the word

--- frontend/src/screens/App/screens/SubmitStoryWizard/components/IntroGraph.tsx
+++ frontend/src/screens/App/screens/SubmitStoryWizard/components/IntroGraph.tsx
@@ -3,9 +3,9 @@ import React from 'react';
 export default function IntroGraph(): JSX.Element {
   return (
     <p>
-      Whether you lived here, you&rsquo;re a historian, or it&rsquo;s the set of
-      your favorite New York novel, I&rsquo;d love if you shared your memories
-      and knowledge of this area with <i>1940s.nyc</i> visitors.
+      Whether you lived here, you&rsquo;re a historian, or it&rsquo;s the
+      setting of your favorite New York novel, I&rsquo;d love if you shared your
+      memories and knowledge of this area with <i>1940s.nyc</i> visitors.
     </p>
   );
 }
