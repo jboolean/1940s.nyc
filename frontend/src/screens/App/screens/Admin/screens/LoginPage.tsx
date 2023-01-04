562f4fbcad4d1df79d8f88ab69d7126a6ccb1571
Julian Boilen <julianboilen@gmail.com>
2023-01-03 19:13:36 -0500

    It was losing track of the return to location

--- frontend/src/screens/App/screens/Admin/screens/LoginPage.tsx
+++ frontend/src/screens/App/screens/Admin/screens/LoginPage.tsx
@@ -2,16 +2,18 @@ import React from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
 import Button from 'shared/components/Button';
 import useAuthStore from 'shared/stores/AuthStore';
+import { LocationDescriptor } from 'history';
 
 export default function LoginPage(): JSX.Element {
   const login = useAuthStore((state) => state.login);
   const close = useAuthStore((state) => state.close);
   const isAutheticated = useAuthStore((state) => state.isAutheticated);
+  const returnToRoute = useAuthStore((state) => state.returnToRoute);
 
   const location = useLocation<{ from?: string }>();
   const history = useHistory();
 
-  const { from } = location.state || { from: { pathname: '/' } };
+  const from: LocationDescriptor = location.state?.from || returnToRoute || '/';
 
   // If authenticated, redirect to the page they were trying to access
   React.useEffect(() => {
