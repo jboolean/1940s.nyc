726f18334988b01e5b67babb7224d1f50be6a57c
Julian Boilen <julianboilen@gmail.com>
2023-02-05 18:40:12 -0500

    Ensure JWT_SECRET is present to fix typing

--- backend/src/business/stories/StoryTokenService.ts
+++ backend/src/business/stories/StoryTokenService.ts
@@ -1,16 +1,15 @@
 import jwt from 'jsonwebtoken';
 import Story from '../../entities/Story';
+import required from '../utils/required';
 
-const STAGE = process.env.STAGE;
+const STAGE = required(process.env.STAGE, 'process.env.STAGE');
 
-if (!STAGE) {
-  throw new Error('process.env.STAGE is not set');
-}
+const JWT_SECRET = required(process.env.JWT_SECRET, 'process.env.JWT_SECRET');
 
 const ISSUER = `${STAGE}.1940s.nyc`;
 
 export function createStoryToken(storyId: Story['id']): string {
-  return jwt.sign({}, process.env.JWT_SECRET, {
+  return jwt.sign({}, JWT_SECRET, {
     algorithm: 'HS256',
     issuer: ISSUER,
     subject: storyId.toString(),
@@ -22,7 +21,7 @@ export function verifyStoryToken(
   expectedStoryId: Story['id']
 ): boolean {
   try {
-    jwt.verify(token, process.env.JWT_SECRET, {
+    jwt.verify(token, JWT_SECRET, {
       algorithms: ['HS256'],
       issuer: ISSUER,
       subject: expectedStoryId.toString(),
@@ -36,7 +35,7 @@ export function verifyStoryToken(
 
 export function getStoryFromToken(token: string): Story['id'] | undefined {
   try {
-    const { sub } = jwt.verify(token, process.env.JWT_SECRET, {
+    const { sub } = jwt.verify(token, JWT_SECRET, {
       algorithms: ['HS256'],
       issuer: ISSUER,
     }) as { sub: string };
