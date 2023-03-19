739bfef45a1c6a7bef3fcea7faa0943ffc8b8c23
Julian Boilen <julianboilen@gmail.com>
2023-03-18 21:43:18 -0400

    Use AI to backfill titles for stories

---
+++ backend/src/cron/generateStoryTitles.ts
@@ -0,0 +1,32 @@
+import { IsNull } from 'typeorm';
+import StoryRepository from '../repositories/StoryRepository';
+import * as AiService from '../business/ai/AiService';
+
+const LIMIT = 20;
+
+export default async function generateStoryTitles(): Promise<void> {
+  const repository = StoryRepository();
+  const stories = await repository
+    .createQueryBuilder('story')
+    .where({ title: IsNull() })
+    .limit(LIMIT)
+    .getMany();
+
+  stories.map(async (story) => {
+    const textContent = story.textContent;
+    if (!textContent) return;
+
+    try {
+      const suggestedTitle = await AiService.suggestStoryTitle(textContent);
+
+      await repository.update(story.id, {
+        title: suggestedTitle,
+        updatedAt: story.updatedAt,
+      });
+    } catch (e) {
+      console.warn('Could not generate title for story', story.id, e);
+    }
+  });
+
+  await Promise.all(stories);
+}
