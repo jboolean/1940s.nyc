ddaca115cb64744b6f6931cbba31564fb9c1c5fd
Julian Boilen <julianboilen@gmail.com>
2023-07-01 15:27:25 -0400

    New tip amounts

--- frontend/src/screens/App/screens/TipJar/useAmountPresets.ts
+++ frontend/src/screens/App/screens/TipJar/useAmountPresets.ts
@@ -1,16 +1,18 @@
-import { useExperimentVariants } from 'shared/utils/OptimizeExperiments';
+// import { useExperimentVariants } from 'shared/utils/OptimizeExperiments';
 
-const PRESET_OPTIONS_BY_VARIANT: Record<number, number[]> = {
-  0: [2, 8, 16, 20],
-  1: [5, 10, 20, 40],
-};
+// const PRESET_OPTIONS_BY_VARIANT: Record<number, number[]> = {
+//   0: [2, 8, 16, 20],
+//   1: [5, 10, 20, 40],
+// };
 
 export default function useAmountPresets(): number[] {
-  const [presetsVariant] = [
-    useExperimentVariants('wLWeVH_USHuTlkUtAK9Erw'),
-    [0],
-  ].find((v) => Array.isArray(v));
-  const amountPresets = PRESET_OPTIONS_BY_VARIANT[presetsVariant];
+  // const [presetsVariant] = [
+  //   useExperimentVariants('wLWeVH_USHuTlkUtAK9Erw'),
+  //   [0],
+  // ].find((v) => Array.isArray(v));
+  // const amountPresets = PRESET_OPTIONS_BY_VARIANT[presetsVariant];
 
-  return amountPresets;
+  // return amountPresets;
+
+  return [2, 10, 20, 40];
 }
