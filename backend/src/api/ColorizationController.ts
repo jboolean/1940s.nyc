2cc498c08ead70ff95a062869c31dcf3343a4f76
Julian Boilen <julianboilen@gmail.com>
2023-07-03 13:09:02 -0400

    Endpoint to go to credit purchase checkout

--- backend/src/api/ColorizationController.ts
+++ backend/src/api/ColorizationController.ts
@@ -1,7 +1,31 @@
 import * as express from 'express';
-import { Controller, Get, Path, Request, Route, Security } from 'tsoa';
+import {
+  Body,
+  Controller,
+  Get,
+  Path,
+  Post,
+  Request,
+  Route,
+  Security,
+} from 'tsoa';
 import * as ColorService from '../business/color/ColorService';
+import * as UserService from '../business/users/UserService';
+import isProduction from '../business/utils/isProduction';
 import { getUserFromRequestOrCreateAndSetCookie } from './auth/userAuthUtils';
+import stripe from './stripe';
+
+type BuyCreditsSessionRequest = {
+  quantity: number;
+  successUrl: string;
+  cancelUrl: string;
+};
+
+const COLOR_CREDIT_PRICE_ID = isProduction()
+  ? 'price_TODO' // TODO
+  : 'price_1NPpdrFCLBtNZLVlpl2852ha';
+
+const MIN_QUANTITY = 20;
 
 @Route('colorization')
 export class ColorizationController extends Controller {
@@ -20,5 +44,55 @@ export class ColorizationController extends Controller {
 
     const url = await ColorService.getColorizedImage(identifier, userId);
     res.redirect(301, url);
+  }
+
+  @Security('user-token')
+  @Post('/billing/buy-credits/sessions')
+  public async createBuyCreditsSession(
+    @Body() body: BuyCreditsSessionRequest,
+    @Request() req: express.Request
+  ): Promise<{ sessionId: string }> {
+    const { quantity, successUrl, cancelUrl } = body;
+    const userId = await getUserFromRequestOrCreateAndSetCookie(req);
+
+    const user = await UserService.getUser(userId);
+
+    const stripeCustomerId: string | undefined =
+      user?.stripeCustomerId ?? undefined;
+    const hasExistingCustomer = !!stripeCustomerId;
+
+    try {
+      const session = await stripe.checkout.sessions.create({
+        cancel_url: cancelUrl,
+        mode: 'payment',
+        success_url: successUrl,
+        line_items: [
+          {
+            price: COLOR_CREDIT_PRICE_ID,
+            quantity,
+            adjustable_quantity: {
+              enabled: true,
+              minimum: MIN_QUANTITY,
+            },
+          },
+        ],
+        metadata: {
+          userId,
+        },
+        customer: stripeCustomerId,
+        customer_creation: hasExistingCustomer ? undefined : 'always',
+        customer_email: hasExistingCustomer
+          ? undefined
+          : user?.email ?? undefined,
+        payment_intent_data: {
+          setup_future_usage: 'on_session',
+        },
+      });
+
+      return { sessionId: session.id };
+    } catch (err) {
+      console.error('Error creating Stripe session', err);
+      throw err;
+    }
   }
 }
