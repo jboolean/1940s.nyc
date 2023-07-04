2105242463a78d78171f63f675c617b19396b93d
Julian Boilen <julianboilen@gmail.com>
2023-07-04 15:13:59 -0400

    remove query from qurl

--- frontend/src/screens/App/screens/CreditPurchaseModal/SuccessMessage.tsx
+++ frontend/src/screens/App/screens/CreditPurchaseModal/SuccessMessage.tsx
@@ -1,10 +1,21 @@
 import Modal from 'components/Modal';
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 export default function ThankYou({
   isOpen,
   onRequestClose,
 }: Pick<ReactModal.Props, 'isOpen' | 'onRequestClose'>): JSX.Element {
+  const history = useHistory();
+
+  React.useEffect(() => {
+    // Remove query from url
+    history.replace({
+      pathname: history.location.pathname,
+      hash: history.location.hash,
+    });
+  }, [history]);
+
   return (
     <Modal size="small" isOpen={isOpen} onRequestClose={onRequestClose}>
       <h1>Purchase successful</h1>
