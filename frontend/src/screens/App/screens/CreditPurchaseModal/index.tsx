ad00faa2e77192d6f48af08041cfc3ac23961a5b
Julian Boilen <julianboilen@gmail.com>
2023-09-04 14:01:18 -0400

    Reduce to one free usage per day

--- frontend/src/screens/App/screens/CreditPurchaseModal/index.tsx
+++ frontend/src/screens/App/screens/CreditPurchaseModal/index.tsx
@@ -50,10 +50,10 @@ export default function CreditPurchaseModal(): JSX.Element {
 
         <div>
           <p>
-            Enjoy two free colorized photos per day using the best AI
-            colorization model available. After that, a small fee (equal to a
-            1948 subway ride) is required to cover the costs of the model.
-            Purchase tokens to keep exploring in color and support the site.
+            Enjoy one free colorized photo per day using the best AI
+            colorization model available. After that, a small fee is required to
+            cover the costs of the model. Purchase tokens to keep exploring in
+            color and support the site.
           </p>
 
           <p className={stylesheet.finePrint}>
