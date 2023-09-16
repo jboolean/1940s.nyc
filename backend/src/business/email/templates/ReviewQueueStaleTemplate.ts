23451d5f950f64919c97b457193831bf1e72b02b
Julian Boilen <julianboilen@gmail.com>
2023-09-16 11:01:14 -0400

    Include reviewer stats in stale stories email

--- backend/src/business/email/templates/ReviewQueueStaleTemplate.ts
+++ backend/src/business/email/templates/ReviewQueueStaleTemplate.ts
@@ -5,6 +5,7 @@ import Senders from './Senders';
 type StoryEmailTemplateData = {
   storiesCount: number;
   reviewStoriesUrl: string;
+  stats: { reviewer: string; count: number }[];
 };
 
 class ReviewQueueStaleTemplate extends EmailTemplate<
