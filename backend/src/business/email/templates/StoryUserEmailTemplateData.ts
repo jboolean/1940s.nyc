b00951de19e7010efd39194e2c26efc00021e3ba
Julian Boilen <julianboilen@gmail.com>
2023-09-16 12:49:02 -0400

    Add map and image to story email

--- backend/src/business/email/templates/StoryUserEmailTemplateData.ts
+++ backend/src/business/email/templates/StoryUserEmailTemplateData.ts
@@ -3,6 +3,9 @@ export interface StoryEmailTemplateData {
   photoDescription: string;
   storyEditUrl: string;
   viewPhotoUrl: string;
+  photoThumbnailUrl: string;
+  mapImageUrl: string | null;
+  mapImageUrlRetina: string | null;
 }
 
 export interface StoryEmailMetadata {
