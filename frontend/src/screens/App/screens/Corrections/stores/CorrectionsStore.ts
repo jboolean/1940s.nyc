e6798a0782b1c610d5fa388f23352925acfa5c56
Julian Boilen <julianboilen@gmail.com>
2023-12-28 11:59:09 -0500

    Select on Map

--- frontend/src/screens/App/screens/Corrections/stores/CorrectionsStore.ts
+++ frontend/src/screens/App/screens/Corrections/stores/CorrectionsStore.ts
@@ -4,19 +4,33 @@ import { immer } from 'zustand/middleware/immer';
 import useLoginStore from 'shared/stores/LoginStore';
 import { getAlternatePhotos, getPhoto, Photo } from 'shared/utils/photosApi';
 
+type LngLat = [number, number];
+
+const DEFAULT_LNG_LAT: LngLat = [-73.99397, 40.7093];
+
 interface State {
   isOpen: boolean;
   photoId: string | null;
   photo: Photo | null;
   alternatesSelections: Record<string, boolean>;
+
+  isMapOpen: boolean;
+  correctedLng: number | null;
+  correctedLat: number | null;
 }
 
-interface ComputedState {}
+interface ComputedState {
+  defaultLng: number;
+  defaultLat: number;
+}
 
 interface Actions {
   initialize: (photo: string) => void;
   close: () => void;
   toggleAlternateSelection: (identifier: string) => void;
+  openMap: () => void;
+  closeMap: () => void;
+  setCorrectedLngLat: (lng: number, lat: number) => void;
 }
 
 const useStoryDraftStore = create(
@@ -25,6 +39,9 @@ const useStoryDraftStore = create(
     photoId: null,
     photo: null,
     alternatesSelections: {},
+    isMapOpen: false,
+    correctedLng: null,
+    correctedLat: null,
 
     initialize: (photo: string) => {
       set((draft) => {
@@ -32,6 +49,9 @@ const useStoryDraftStore = create(
         draft.isOpen = true;
         draft.photo = null;
         draft.alternatesSelections = {};
+        draft.isMapOpen = false;
+        draft.correctedLng = null;
+        draft.correctedLat = null;
       });
 
       useLoginStore.getState().initialize();
@@ -72,11 +92,36 @@ const useStoryDraftStore = create(
           !get().alternatesSelections[identifier];
       });
     },
+
+    openMap: () => {
+      set((draft) => {
+        draft.isMapOpen = true;
+      });
+    },
+    closeMap: () => {
+      set((draft) => {
+        draft.isMapOpen = false;
+      });
+    },
+    setCorrectedLngLat: (lng: number | null, lat: number | null) => {
+      set((draft) => {
+        // round to 6 decimal places
+        draft.correctedLng = lng;
+        draft.correctedLat = lat;
+      });
+    },
   }))
 );
 
 export function useStoryDraftStoreComputeds(): ComputedState {
-  return {};
+  const { photo } = useStoryDraftStore();
+  const defaultGeocode = photo?.geocodeResults?.find(
+    (result) => !!result.lngLat
+  );
+  return {
+    defaultLng: defaultGeocode?.lngLat.lng ?? DEFAULT_LNG_LAT[0],
+    defaultLat: defaultGeocode?.lngLat.lat ?? DEFAULT_LNG_LAT[1],
+  };
 }
 
 export default useStoryDraftStore;
