f54f60b416dac53abda4859df0001c81fe33ae76
Julian Boilen <julianboilen@gmail.com>
2024-12-22 16:42:30 -0500

    Ignore bad touches on overlay

--- frontend/src/screens/App/screens/ViewerPane/components/Overlay/index.tsx
+++ frontend/src/screens/App/screens/ViewerPane/components/Overlay/index.tsx
@@ -19,6 +19,10 @@ export default function Overlay({
   // This feature flag is useful in development to prevent the overlay from disappearing
   const alwaysShowOverlay = useFeatureFlag(FeatureFlag.ALWAYS_SHOW_OVERLAY);
 
+  const startXRef = React.useRef<number>(0);
+  const startYRef = React.useRef<number>(0);
+  const pointerIdRef = React.useRef<number | null>(null);
+
   const [isOverlayVisible, setIsOverlayVisible] =
     React.useState(alwaysShowOverlay);
   const timeoutRef = React.useRef<number | null>(null);
@@ -49,11 +53,32 @@ export default function Overlay({
     if (e.pointerType !== 'mouse') {
       return;
     }
+    console.log('handleHoverStart', e);
     setIsOverlayVisible(true);
     cancelPeekTimeout();
   };
 
-  const handlePointerUp: React.PointerEventHandler<HTMLDivElement> = () => {
+  const handlePointerDown: React.PointerEventHandler<HTMLDivElement> = (e) => {
+    console.log('handlePointerDown', e);
+    pointerIdRef.current = e.pointerId;
+    startXRef.current = e.clientX;
+    startYRef.current = e.clientY;
+  };
+
+  const handlePointerUp: React.PointerEventHandler<HTMLDivElement> = (e) => {
+    console.log('handlePointerUp', e);
+    if (e.pointerId !== pointerIdRef.current) {
+      console.log('pointerId mismatch');
+      return;
+    }
+    const deltaX = e.clientX - startXRef.current;
+    const deltaY = e.clientY - startYRef.current;
+    const distance = Math.sqrt(deltaX ** 2 + deltaY ** 2);
+    // If the pointer moved more than 10 pixels, ignore
+    if (distance > 10) {
+      console.log('distance too far');
+      return;
+    }
     setIsOverlayVisible(!isOverlayVisible);
     cancelPeekTimeout();
   };
@@ -64,6 +89,7 @@ export default function Overlay({
     if (e.pointerType !== 'mouse') {
       return;
     }
+    console.log('handleEndHover', e);
     if (alwaysShowOverlay) {
       return;
     }
@@ -75,7 +101,11 @@ export default function Overlay({
       className={classnames(stylesheet.overlay, className, {})}
       onPointerOver={handleStartHover}
       onPointerLeave={handleEndHover}
+      onPointerDown={handlePointerDown}
       onPointerUp={handlePointerUp}
+      onPointerCancel={(e) => {
+        console.log('handlePointerCancel', e);
+      }}
       ref={overlayRef}
     >
       <CSSTransition
