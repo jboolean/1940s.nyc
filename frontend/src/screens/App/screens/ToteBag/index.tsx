cfa6eaacd4af5c3756df295bcad9b989918a5704
Julian Boilen <julianboilen@gmail.com>
2025-02-07 14:58:37 -0500

    fix: update attribution text to reflect correct mapping

--- frontend/src/screens/App/screens/ToteBag/index.tsx
+++ frontend/src/screens/App/screens/ToteBag/index.tsx
@@ -54,10 +54,7 @@ export default function ToteBag(): JSX.Element {
             </span>
           </div>
           <div className={stylesheet.attribution}>
-            {/* I can't get the correct attributions to show based on what's in the viewport, it just shows all of them, so we say "May show" */}
-            <span className={stylesheet.hugLines}>
-              May show: {attributionText}
-            </span>
+            <span className={stylesheet.hugLines}>Maps: {attributionText}</span>
           </div>
         </div>
       </div>
