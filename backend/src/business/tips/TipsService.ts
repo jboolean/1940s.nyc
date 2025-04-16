38b6afe09c7d2bed3ec2d81c490eaa9818c8bcb1
Julian Boilen <julianboilen@gmail.com>
2025-04-16 15:13:57 -0400

    Remove debug statement

--- backend/src/business/tips/TipsService.ts
+++ backend/src/business/tips/TipsService.ts
@@ -35,7 +35,6 @@ export async function createTipCheckoutSession({
   frequency: TipFrequency;
   gift?: GiftRegistry.Gift;
 }): Promise<string> {
-  console.log('Creating tip checkout session', user);
   const userId = user?.id;
   const stripeCustomerId: string | undefined =
     user?.stripeCustomerId ?? undefined;
