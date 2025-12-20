01dc9d2c5e6e937d2982997ab6f106bb5edd287b
Alexander Garcia <agarcia1359@gmail.com>
2025-12-20 10:41:58 -0500

    feat(tiptap): remove Markdown extension, heading support
    Markdown extension is really only needed for making the TipTap data model more aware of markdown (pasting from markdown, exporting as markdown, etc). The StarterKit extension already provides support for:
    - *text* (italics)
    - **text** (bold)
    
    Also:
    - add ctrl+k/cmd+k shortcut to toggle links
    - remove support for heading tags and lists

--- frontend/src/shared/components/TipTapEditor/index.tsx
+++ frontend/src/shared/components/TipTapEditor/index.tsx
@@ -1,5 +1,4 @@
-import { Markdown } from '@tiptap/markdown';
-import { EditorContent, useEditor } from '@tiptap/react';
+import { EditorContent, Extension, useEditor } from '@tiptap/react';
 import Placeholder from '@tiptap/extension-placeholder';
 import StarterKit from '@tiptap/starter-kit';
 import React from 'react';
@@ -15,6 +14,36 @@ interface TipTapEditorProps {
   onChange?: (newContent: string) => void;
 }
 
+const ToggleLinkShortcut = Extension.create({
+  name: 'toggleLinkShortcut',
+
+  addKeyboardShortcuts() {
+    return {
+      'Mod-k': () => {
+        const { editor } = this;
+
+        // Disable link if is active
+        if (editor.isActive('link')) {
+          return editor.commands.unsetLink();
+        }
+
+        const selectedUrl = editor.state.doc.textBetween(
+          editor.state.selection.from,
+          editor.state.selection.to
+        );
+
+        if (selectedUrl) {
+          return editor.commands.setLink({
+            href: selectedUrl,
+            target: '_blank',
+          });
+        }
+        return true;
+      },
+    };
+  },
+});
+
 export default function TipTapEditor({
   content,
   className,
@@ -23,9 +52,14 @@ export default function TipTapEditor({
   onChange,
 }: TipTapEditorProps): JSX.Element {
   const extensions = [
-    Markdown,
-    StarterKit,
+    StarterKit.configure({
+      // Only support bold, italics, links
+      heading: false,
+      bulletList: false,
+      orderedList: false,
+    }),
     Placeholder.configure({ placeholder }),
+    ToggleLinkShortcut,
   ];
 
   const editor = useEditor({
